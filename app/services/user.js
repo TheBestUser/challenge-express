147fd53cc3a178356ad0adf28b1a15dfcf2d7cfc
thebestuser <kuzmichev.96@gmail.com>
2023-01-18 21:09:00 +0800

    Improved request performance

--- app/services/user.js
+++ app/services/user.js
@@ -1,10 +1,37 @@
 'use strict';
 
-const { User, sequelize } = require('../../db');
+const { User, sequelize, Sequelize } = require('../../db');
 const { AppError } = require('../types');
 
 const UserService = {
+  /**
+   * requests: 10000
+   * ex. time: ~5s
+   */
   updateBalance: async (userId, amount) => {
+    const balance =
+      amount < 0 ? { balance: { [Sequelize.Op.gte]: Math.abs(amount) } } : {};
+
+    const [res] = await User.increment(
+      { balance: amount },
+      { where: { id: userId, ...balance } },
+    );
+
+    const [[data], count] = res;
+
+    if (!count) {
+      throw new AppError('Unable to update balance', 403);
+    }
+
+    return data;
+  },
+
+  /**
+   * too slow
+   * requests: 10000
+   * ex. time: ~25s
+   */
+  updateBalanceSlow: async (userId, amount) => {
     return await sequelize.transaction(async transaction => {
       const user = await User.findByPk(userId, { transaction, lock: true });
 
