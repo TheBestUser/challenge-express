ad140d938d6735adae1b1a98127a6caed3d773b3
thebestuser <kuzmichev.96@gmail.com>
2023-01-18 21:10:10 +0800

    Added 'Not Found' API handler

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,13 @@
 'use strict';
 
+const { AppError } = require('../types');
 const router = require('express').Router();
 
+function notFound(req, res, next) {
+  next(new AppError('Not Found', 404));
+}
+
 router.use('/api', require('./api'));
+router.use('/api', notFound);
 
 module.exports = router;
